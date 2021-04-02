886e6ef6af8e6505926ab4c1de4fecb936af1b53
Remco Gubbels <remco090@gmail.com>
2021-04-02 14:29:26 +0200

    ci: add summonerData call

--- lol-clash-roles/index.ts
+++ lol-clash-roles/index.ts
@@ -1,14 +1,16 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
 import { decryptSecret } from './utils';
+import * as fetch from 'node-fetch';
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     // TODO implement
 
     const apiKey = await decryptSecret('RIOT_API_KEY');
 
+    const summonerdata = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${event.queryStringParameters.summonerName}?api_key=${apiKey}`).then(res => res.json())
     const response = {
         statusCode: 200,
-        body: JSON.stringify(`Hello from Bub from typescript - ${apiKey}! - ${event.queryStringParameters.summonerName}`),
+        body: JSON.stringify(summonerdata),
     };
     return response;
 };
