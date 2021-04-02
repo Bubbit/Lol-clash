f573da03136747d26ce7db8f1f67a2bdeb0151d5
Remco Gubbels <remco090@gmail.com>
2021-04-02 12:45:35 +0200

    chore: example secrets

--- lol-clash-roles/index.ts
+++ lol-clash-roles/index.ts
@@ -2,9 +2,13 @@ import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lamb
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
     // TODO implement
+
+    const secret1 = '[first secret]';
+    const secret2 = '[second secret]';
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Bub from typescript!'),
+        body: JSON.stringify(`Hello from Bub from typescript - ${secret1} - ${secret2}!`),
     };
     return response;
 };
