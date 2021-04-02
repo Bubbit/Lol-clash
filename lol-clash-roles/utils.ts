61b608f0e5bc49381bb33cb358d8fd5ad6535c82
Remco Gubbels <remco090@gmail.com>
2021-04-02 13:12:31 +0200

    fix: add encryptionContext to decrypt function

--- lol-clash-roles/utils.ts
+++ lol-clash-roles/utils.ts
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'eu-west-2' });
+const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
 const kms = new AWS.KMS();
 
 const decrypted = {};
@@ -11,7 +12,10 @@ export const decryptSecret = async (secretName) => {
   }
   console.log('decrypting ' + secretName);
   try {
-    const req = { CiphertextBlob: Buffer.from(process.env[secretName], 'base64') };
+    const req = { 
+        CiphertextBlob: Buffer.from(process.env[secretName], 'base64'),
+        EncryptionContext: { LambdaFunctionName: functionName },
+    };
     const data = await kms.decrypt(req).promise();
     const decryptedVal = data.Plaintext.toString('ascii');
     decrypted[secretName] = decryptedVal;
