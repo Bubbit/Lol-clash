53a586d7d8759616993ec2ea59b0b6998959e86c
Remco Gubbels <remco090@gmail.com>
2021-04-03 10:18:10 +0200

    feat: add clashID fetch and clashTeam

--- lol-clash-roles/index.ts
+++ lol-clash-roles/index.ts
@@ -3,14 +3,18 @@ import { decryptSecret } from './utils';
 import * as fetch from 'node-fetch';
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-    // TODO implement
-
     const apiKey = await decryptSecret('RIOT_API_KEY');
 
+    // Get summonerID
     const summonerdata = await fetch(`https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${event.queryStringParameters.summonerName}?api_key=${apiKey}`).then(res => res.json())
+    // Get clashTeamID
+    const clashID = await fetch(`https://euw1.api.riotgames.com/lol/clash/v1/players/by-summoner/${summonerdata.id}?api_key=${apiKey}`).then(res => res.json());
+    // Get teamsetup
+    const clashTeam = await fetch(`https://euw1.api.riotgames.com/lol/clash/v1/teams/${clashID[0].teamId}?api_key=${apiKey}`).then(res => res.json());
+    
     const response = {
         statusCode: 200,
-        body: JSON.stringify(summonerdata),
+        body: JSON.stringify(clashTeam),
     };
     return response;
 };
